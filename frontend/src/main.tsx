3805c975e0e91a1fde598686fc10affba2083fd7
Wdyffs <kiruha200146@gmail.com>
2023-05-03 18:10:54 +0300

    add store

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,17 @@ import './index.css';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {RouterProvider} from 'react-router-dom';
 import {router} from './router';
+import {Provider} from "react-redux";
+import store from "./store/store";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}></RouterProvider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
---
+++ frontend/src/store/slices/auth.slice.ts
@@ -0,0 +1,23 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+interface IAuthSlice {
+  isAuth: boolean
+}
+
+const initialState: IAuthSlice = {isAuth: false};
+
+const authSlice = createSlice({
+  name: "isAuth",
+  initialState,
+  reducers: {
+    setAuth(state) {
+      state.isAuth = true
+    },
+    unAuth(state) {
+      state.isAuth = false
+    }
+  }
+})
+
+export const {setAuth, unAuth} = authSlice.actions;
+export default authSlice.reducer;
