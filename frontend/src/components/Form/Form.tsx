e3cafe4a50202c345e737d1aa6627a9ceac5a772
Wdyffs <kiruha200146@gmail.com>
2023-05-04 14:45:29 +0300

    registration functionality

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -1,30 +1,44 @@
-import { KeyboardEventHandler, ReactNode, useEffect } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import FormField from '../InputField/InputContainer';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { FormTypes, IFormInput } from '../../models/FormInput.model';
 import { EFormInput, EFormType } from '../../enums/form/authForm.enum';
-import { ErrorMessage } from '@hookform/error-message';
 import ErrorInput from '../ErrorInput/ErrorInput';
 import { useMutation } from '@tanstack/react-query';
 
 type Props = {
     formType: FormTypes;
+    toggleModal: Dispatch<SetStateAction<boolean>>;
+    setModalType: Dispatch<
+        SetStateAction<EFormType.REGISTER | EFormType.LOGIN>
+    >;
 };
 type RegInfo = {
     login: string;
     password: string;
 };
 
-const Form = ({ formType }: Props) => {
+const Form = ({ formType, toggleModal, setModalType }: Props) => {
     const mutations = useMutation({
         mutationFn: (reginfo: RegInfo) => {
-            return fetch('http://localhost:3000/app/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json; charset=utf-8',
-                },
-                body: JSON.stringify(reginfo),
-            });
+            return formType === EFormType.REGISTER
+                ? fetch('http://localhost:3000/app/register', {
+                      method: 'POST',
+                      headers: {
+                          'Content-Type': 'application/json; charset=utf-8',
+                      },
+                      body: JSON.stringify(reginfo),
+                  })
+                : fetch('http://localhost:3000/app/register', {
+                      method: 'POST',
+                      headers: {
+                          'Content-Type': 'application/json; charset=utf-8',
+                      },
+                      body: JSON.stringify(reginfo),
+                  });
+        },
+        onSuccess: () => {
+            toggleModal(false);
         },
     });
     const {
@@ -34,7 +48,6 @@ const Form = ({ formType }: Props) => {
     } = useForm<IFormInput>({ criteriaMode: 'all' });
 
     const onSubmit: SubmitHandler<IFormInput> = (data) => {
-        console.log(data);
         mutations.mutate(data);
     };
 
@@ -44,7 +57,7 @@ const Form = ({ formType }: Props) => {
             onSubmit={handleSubmit(onSubmit)}
         >
             <legend className='text-amber-300 text-2xl m-auto mb-4'>
-                {formType === 'register'
+                {formType === EFormType.REGISTER
                     ? 'Create an account'
                     : 'Log in to your existing account'}
             </legend>
@@ -88,10 +101,31 @@ const Form = ({ formType }: Props) => {
             {mutations.isLoading && <p>Processing</p>}
             {mutations.isError && <p>Error</p>}
             <input
-                className='mt-4 text-xl bg-pink-700 text-white p-2 rounded-md cursor-pointer'
+                className='mt-4 text-xl bg-pink-700 text-white p-2 rounded-md cursor-pointer hover:text-amber-300'
                 type='submit'
                 value={formType === EFormType.REGISTER ? 'Submit' : 'Log in'}
             />
+            {formType === EFormType.LOGIN ? (
+                <p className='mt-4 text-center'>
+                    <span className='mr-2'>Have no account?</span>
+                    <span
+                        className='underline hover:text-amber-300 cursor-pointer'
+                        onClick={() => setModalType(EFormType.REGISTER)}
+                    >
+                        Create new!!!
+                    </span>
+                </p>
+            ) : (
+                <p className='mt-4 text-center'>
+                    <span className='mr-2'>Have already an account?</span>
+                    <span
+                        className='underline hover:text-amber-300 cursor-pointer'
+                        onClick={() => setModalType(EFormType.LOGIN)}
+                    >
+                        Log in
+                    </span>
+                </p>
+            )}
         </form>
     );
 };
--- frontend/src/components/Form/FormContainer.tsx
+++ frontend/src/components/Form/FormContainer.tsx
@@ -8,19 +8,20 @@ import {
 } from 'react';
 import Form from './Form';
 import { FormTypes } from '../../models/FormInput.model';
+import { EFormType } from '../../enums/form/authForm.enum';
 
 type Props = {
     toggleModal: Dispatch<SetStateAction<boolean>>;
     formType: FormTypes;
+    setModalType: Dispatch<
+        SetStateAction<EFormType.REGISTER | EFormType.LOGIN>
+    >;
 };
 
-const FormContainer = ({ toggleModal, formType }: Props) => {
+const FormContainer = ({ toggleModal, formType, setModalType }: Props) => {
     const blurAreaRef = useRef(null);
     const closeLogin: MouseEventHandler<HTMLElement> = (e) => {
         if (e.target == blurAreaRef.current) toggleModal(false);
-    };
-    const onSubmitForm: FormEventHandler<HTMLElement> = (e) => {
-        e.preventDefault();
     };
     useEffect(() => {
         const handleEsc = (e: KeyboardEvent) => {
@@ -30,7 +31,6 @@ const FormContainer = ({ toggleModal, formType }: Props) => {
         };
         window.addEventListener('keyup', handleEsc);
         return () => {
-            console.log('working');
             window.removeEventListener('keyup', handleEsc);
         };
     }, []);
@@ -39,9 +39,12 @@ const FormContainer = ({ toggleModal, formType }: Props) => {
             className='fixed top-0 bottom-0 w-full flex items-center justify-center z-30 blur-none backdrop-blur-sm'
             ref={blurAreaRef}
             onClick={(e) => closeLogin(e)}
-            onSubmit={onSubmitForm}
         >
-            <Form formType={formType} />
+            <Form
+                formType={formType}
+                toggleModal={toggleModal}
+                setModalType={setModalType}
+            />
         </section>
     );
 };
